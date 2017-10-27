7436bcaf5bb46c07a2c3acdc060cbd60ce733948
Chunjie Zhang (张纯洁) <ChunjieZhang@quark.com>
2017-10-27 16:27:14 +0800

    CJZhang push to Zhangc1626

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("CJZhang1620");
-        System.out.println("CJZhang1624Tozhangc");
+        System.out.println("CJZhang1626Tozhangc");
     }
 }
